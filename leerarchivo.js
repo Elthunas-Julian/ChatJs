03d1877dc735a74869b4be459a7e481d7def8887
Elthunas-Julian <146143935+Elthunas-Julian@users.noreply.github.com>
2025-03-17 19:13:22 -0500

    2
    cambio

--- leerarchivo.js
+++ leerarchivo.js
@@ -1,4 +1,3 @@
-
 const fs = require('fs');
 
 
@@ -7,10 +6,10 @@ const rutaarchivo = 'C:\\Users\\julia\\Downloads\\imagen\\archivo.txt';
 
 fs.readFile(rutaarchivo, 'utf8', (error, datos) => {
   if (error) {
-    console.error('Error al leer el archivo:', error.message);
+    console.error('Los archivos no se pueden leer:', error.message);
     return;
   }
   
-  console.log('Contenido del archivo:');
+  console.log('Datos del archivo:');
   console.log(datos);
 });
